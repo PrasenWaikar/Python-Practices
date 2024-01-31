3dc507968665a5e3bf1b17c1b71709072ed36aa0
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-01-31 12:31:17 +0530

    Add files via upload

---
+++ Binary search.py
@@ -0,0 +1,8 @@
+# Implement a binary search algorithm.
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# target = 6
+n = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+t=4
+for i in n:
+    if i==t:
+        print(n.index(i))
