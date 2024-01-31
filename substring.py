81a03f7ae1bd318a7d2e27465811ede305a4622c
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-01-31 12:31:01 +0530

    Add files via upload

---
+++ substring.py
@@ -0,0 +1,12 @@
+# Program to find the longest substring without repeating characters in a given string.
+# string = "xyzzzyxxyzyx"
+# Output: 3
+string = "wxyzzzwyxxywzwyx"
+l=list(string)
+a=set(l)
+print(len(a))
+
+
+
+
+
