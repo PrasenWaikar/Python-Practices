4281d1e8b473c7db0e84458d4c688c18cd3ab224
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-02-22 17:26:39 +0530

    Add files via upload

---
+++ Nested dictionary.py
@@ -0,0 +1,11 @@
+nested_dict = {
+    'key1': {
+        'subkey1': 'value1',
+        'subkey2': 'value2'
+    },
+    'key2': {
+        'subkey3': 'value3',
+        'subkey4': 'value4'
+    }
+}
+print(nested_dict)
