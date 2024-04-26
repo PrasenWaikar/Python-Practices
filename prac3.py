feda3747cee2395d68a656246bc2b11836d07c90
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-04-27 00:32:16 +0530

    Add files via upload

---
+++ prac3.py
@@ -0,0 +1,16 @@
+""" 
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+"""
+def tar(nums,target):
+    for i in range(len(nums)):
+        for j in range(i+1,len(nums)):
+            if nums[i] + nums[j] ==target:
+                return i,j
+
+
+nums=[2,7,11,15]
+target=9
+check=tar(nums,target)
+print(check)
