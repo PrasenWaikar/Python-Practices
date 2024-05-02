cc28944d2f21401b13dc37c37c05b983e681dd73
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-05-02 23:34:59 +0530

    Add files via upload

---
+++ Programming based on questions/question_23.py
@@ -0,0 +1,11 @@
+"""  
+ Write a method which can calculate square value of number
+
+"""
+def square_value(num):
+    return num**2
+
+# Example usage
+number = 5
+square = square_value(number)
+print(square)
