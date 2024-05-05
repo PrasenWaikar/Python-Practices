86f8fcfd6bc6baf2b64e66bad61a7c1cf33b4bbb
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-05-05 15:14:22 +0530

    Add files via upload

---
+++ Programming based on questions/question_29.py
@@ -0,0 +1,12 @@
+"""  
+Define a function that can accept two strings as input and concatenate them and then print it in console.
+
+"""
+def concatenate_strings(str1, str2):
+    result = str1 + str2
+    print(result)
+
+# Example usage
+string1 = "Hello, "
+string2 = "world!"
+concatenate_strings(string1, string2)
