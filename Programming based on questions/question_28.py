67c22f785a2cfa845e5a7daa54d8b4dd5b498223
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-05-05 15:13:57 +0530

    Add files via upload

---
+++ Programming based on questions/question_28.py
@@ -0,0 +1,11 @@
+"""  
+Define a function that can convert a integer into a string and print it in console.
+
+"""
+def int_to_str(num):
+    str_num = str(num)
+    print(str_num)
+
+# Example usage
+integer = 123
+int_to_str(integer)
