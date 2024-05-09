892f5489fc5f22b1124d3ee947b71b29d4f7a790
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-05-09 15:50:05 +0530

    Add files via upload

---
+++ Programming based on questions/question_35.py
@@ -0,0 +1,10 @@
+"""  
+Define a function which can generate and print a list where the values are square of numbers between 1 and 20 (both included).
+"""
+
+def print_square_list():
+    square_list = [i**2 for i in range(1, 21)]
+    print(square_list)
+
+# Example usage
+print_square_list()
