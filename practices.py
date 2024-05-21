c2ff654b32972d4303e4dd3b79b2bf4be9a2dcbd
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-05-21 10:17:58 +0530

    Add files via upload

---
+++ practices.py
@@ -0,0 +1,37 @@
+# a={"salary":1000,"name":"Prasen","age":23}
+
+# print("sal =",a["salary"])
+
+# a=int(input("enter number one : "))
+# b=int(input("enter number two : "))
+# c=print(f"The addidtion of {a} and {b} is :",a+b)
+# print(c)
+
+# a=int(input("enter number one : "))
+# b=int(input("enter number two : "))
+# c=print("The addidtion of a and b is :",a+b,sep='....')
+# print(c)
+
+# a = int(input("Enter number one: "))
+# b = int(input("Enter number two: "))
+# c = a + b
+# print("The addition of a and b is:", c, sep=' .... ')
+
+
+# a=input("enter first name : ")
+# b=input("enter second name : ")
+# number=eval(input("enter the ph number : "))
+# country_code='+91-'
+# c=(f"My first name is {a},Second name is {b},my ph number is {country_code}{number}")
+# print(c)
+
+# a=eval(input("enter number : "))
+# b=eval(input("enter number : "))
+# print(a+b)
+
+
+# a="088"
+# print(a.rjust(4,'o'))
+
+
+
