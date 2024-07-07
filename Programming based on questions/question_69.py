1215ec29c36589a6dfc27f311e4a7eddb1867289
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-07-07 18:38:44 +0530

    Add files via upload

---
+++ Programming based on questions/question_69.py
@@ -0,0 +1,5 @@
+# Please generate a random float where the value is between 10 and 100 using Python math module.
+import random
+
+random_float = random.uniform(10, 100)
+print(f"Random float between 10 and 100: {random_float}")
