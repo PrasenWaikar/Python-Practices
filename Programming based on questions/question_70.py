4a456a11d0b779c2693b93fb87b390ea299c1a2e
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-07-09 20:08:17 +0530

    Add files via upload

---
+++ Programming based on questions/question_70.py
@@ -0,0 +1,8 @@
+#Please generate a random float where the value is between 5 and 95 using Python math module.
+
+
+
+import random
+
+random_float = random.uniform(5, 95)
+print(f"Random float between 5 and 95: {random_float}")
