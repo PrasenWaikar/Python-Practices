abe19e80b597463946d6514a3ef92a84cf5c956a
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-08-12 11:39:27 +0530

    Add files via upload

---
+++ number to words.py
@@ -0,0 +1,9 @@
+from num2words import num2words
+
+# Most common usage.
+print(num2words(11098))
+
+# can also specify this for -th format
+print(num2words(11098, to='ordinal'))
+
+//This code will not run on normal compiler, You need to install num2words.
