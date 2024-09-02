5879945140ca55b5d5e8d4872a165c3a8104c5e5
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-09-02 13:29:47 +0530

    Add files via upload

---
+++ Move all the negative elements to one side of the array.py
@@ -0,0 +1,11 @@
+def find(arr):
+    # sort array
+    arr.sort()
+
+    # print array
+    print("Array after moving all the elements to left:", arr)
+
+
+array = [1, 3, -1, 4, -3, -5, -6, 3, 7]
+# call function
+find(array)
