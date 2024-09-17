b59555afbef1f5470d60b32e927a36d55456e4f7
PrasenWaikar <107995139+PrasenWaikar@users.noreply.github.com>
2024-09-17 17:14:39 +0530

    Add files via upload

---
+++ Write a Python program to add a key to a dictionary.py
@@ -0,0 +1,8 @@
+# Sample dictionary
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+
+# Adding a new key-value pair to the dictionary
+my_dict['d'] = 4
+
+# Output the updated dictionary
+print("Updated dictionary:", my_dict)
